1fbe406d118f8fbddf208d662b74c37014a324ff
Clint Leach <clint.leach@gmail.com>
2018-10-15 11:12:11 -0600

    Dopping r and c for basal species

--- src/model.jl
+++ src/model.jl
@@ -2,19 +2,37 @@
 @with_kw struct model
 
     foodweb::Matrix{Int64}
-    S::Int64 = size(foodweb)[1]
-
+    S::Int64
+    basal::Int64
+    nonbasal::Int64
+    reorder::Vector{Int64}
 end
 
+function model(foodweb::Matrix{Int64})
+
+    # Sort the species by number of prey
+    nprey = sum(foodweb, dims = 1)
+    cols = sort([1:length(nprey);], by = i -> nprey[i])
+
+    # Identify number of basal species
+    basal = sum(nprey .== 0)
+
+    model(foodweb = foodweb[cols, cols],
+          S = size(foodweb, 1),
+          basal = basal,
+          nonbasal = size(foodweb, 1) - basal,
+          reorder = cols)
+
+end
 
 function computeLL(θ, m)
 
     loglik = eltype(θ[1])(0.0)
 
-    @inbounds for pred in 1:m.S
+    @inbounds for pred in 1:m.nonbasal
         for prey in 1:m.S
             ϕ = exp(-((θ.n[prey] - θ.c[pred]) / (θ.r[pred] / 2)) ^ 2)
-            loglik += logpdf(Bernoulli(ϕ), m.foodweb[prey, pred])
+            loglik += logpdf(Bernoulli(ϕ), m.foodweb[prey, m.basal + pred])
         end
     end
 
@@ -31,4 +49,5 @@ function (m::model)(θ)
     logpost += logpdf.(Beta(1, 1), θ.r) |> sum
     logpost += logpdf.(Beta(1, 1), θ.c) |> sum
 
+    return logpost
 end
--- src/run.jl
+++ src/run.jl
@@ -6,32 +6,45 @@ using LogDensityProblems
 using DynamicHMC
 using ForwardDiff
 using BenchmarkTools
+using Plots
 
 include("model.jl")
 include("generate.jl")
 
-S = 50
+S = 10
 
 nicheweb = niche_network(S, 0.1)
 
-m = model(foodweb = nicheweb.fw)
+heatmap(nicheweb.fw, legend = false)
 
-θ = (n = rand(Beta(1, 1), S),
-     r = rand(Beta(1, 1), S),
-     c = rand(Beta(1, 1), S))
+m = model(nicheweb.fw)
+
+heatmap(m.foodweb, legend = false)
+
+θ = (n = nicheweb.n[m.reorder],
+     r = nicheweb.r[m.reorder[(m.basal + 1):end]],
+     c = nicheweb.c[m.reorder[(m.basal + 1):end]])
 
 m(θ)
 
-toθ = as((n = as(Array, as𝕀, S),
-          r = as(Array, as𝕀, S),
-          c = as(Array, as𝕀, S)))
+toθ = as((n = as(Array, as𝕀, m.S),
+          r = as(Array, as𝕀, m.nonbasal),
+          c = as(Array, as𝕀, m.nonbasal)))
 
 lp_t = TransformedLogDensity(toθ, m)
-lp∇ = ForwardDiffLogDensity(lp_t, chunk = ForwardDiff.Chunk{S}())
+lp∇ = ForwardDiffLogDensity(lp_t, chunk = ForwardDiff.Chunk{24}())
 
 inits = inverse(toθ, θ)
 logdensity(LogDensityProblems.ValueGradient, lp∇, inits)
 
-chain, smplr = NUTS_init_tune_mcmc(lp∇, 2000, max_depth = 10)
+chain, smplr = NUTS_init_tune_mcmc(lp∇, 500, max_depth = 10)
 
 NUTS_statistics(chain)
+
+posterior = transform.(Ref(lp∇.transformation), get_position.(chain))
+
+n = mean(first, posterior)
+
+foo = zip(posterior...) |> collect
+
+scatter(θ.n, n)
