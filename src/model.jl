1ec06f9351b8982d20c4b41bcfe4c9b025074fd8
Clint Leach <clint.leach@gmail.com>
2018-10-18 17:57:23 -0600

    Dropping n for top predators

--- src/model.jl
+++ src/model.jl
@@ -5,6 +5,8 @@
     S::Int64
     basal::Int64
     nonbasal::Int64
+    top::Int64
+    nontop::Int64
     reorder::Vector{Int64}
 end
 
@@ -13,18 +15,27 @@ function model(foodweb::Matrix{Int64})
     # Prune prey with only specialist prey
     foodweb = prune(foodweb)
 
-    # Sort the species by number of prey
-    nprey = sum(foodweb, dims = 1)
-    cols = sort([1:length(nprey);], by = i -> nprey[i])
+    # Identify basal species
+    nprey = sum(foodweb, dims = 1)[1, :]
+    basal = findall(nprey .== 0)
 
-    # Identify number of basal species
-    basal = sum(nprey .== 0)
+    # Identify top predators
+    npred = sum(foodweb, dims = 2)[:, 1]
+    top = findall(npred .== 0)
 
-    model(foodweb = foodweb[cols, cols],
+    # Identify everyone else
+    middle = findall(x -> !(x in basal) && !(x in top), 1:S)
+
+    # Sort the species
+    order = vcat(basal, middle, top)
+
+    model(foodweb = foodweb[order, order],
           S = size(foodweb, 1),
-          basal = basal,
-          nonbasal = size(foodweb, 1) - basal,
-          reorder = cols)
+          basal = length(basal),
+          nonbasal = size(foodweb, 1) - length(basal),
+          top = length(top),
+          nontop = size(foodweb, 1) - length(top),
+          reorder = order)
 
 end
 
@@ -33,8 +44,8 @@ function computeLL(θ, m)
     loglik = eltype(θ[1])(0.0)
 
     @inbounds for pred in 1:m.nonbasal
-        for prey in 1:m.S
-            ϕ = 0.99 * exp(-((θ.n[prey] - θ.c[pred]) / (θ.r[pred] / 2)) ^ 2)
+        for prey in 1:m.nontop
+            ϕ = 0.99 * exp(-((θ.n[prey] - θ.c[pred] * θ.n[m.basal + pred]) / (θ.r[pred] / 2)) ^ 2)
             loglik += logpdf(Bernoulli(ϕ), m.foodweb[prey, m.basal + pred])
         end
     end
@@ -46,7 +57,7 @@ function (m::model)(θ)
 
     logpost = computeLL(θ, m)
 
-    logpost += logpdf.(Beta(1, 5), θ.r) |> sum
+    logpost += logpdf.(Beta(2, 8), θ.r) |> sum
 
     isnan(logpost) && return -Inf
 
